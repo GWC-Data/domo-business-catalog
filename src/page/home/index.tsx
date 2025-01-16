3d00a1b0827406837eb54df24851f5e87830e1df
vishwanath231-GWC <vishwanath.a@gwcdata.ai>
2025-01-16 10:12:29 +0530

    homepage

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
+import "./home.css";
 
 const HomePage = () => {
   return (
@@ -134,58 +135,55 @@ const HomePage = () => {
         </figure>
       </section>
 
-      <section className="service-section" >
-        <figure className="service-dotimage mb-0">
-          <img src="./assets/images/service-dotimage.png" alt="" className="img-fluid" />
+      {/* service-section */}
+      <section className="gwc_domo-section" >
+        <figure className="gwc_domo-dotimage mb-0">
+          <img src="./assets/images/gwc_domo-dotimage.png" alt="" className="img-fluid" />
         </figure>
         <div className="container">
           <div className="row">
             <div className="col-lg-6 col-md-12 col-sm-12 col-12 order-md-1 order-2">
-              <div className="service_contentbox">
+              <div className="gwc_domo_contentbox">
                 <div className="row">
                   <div className="col-lg-6 col-md-6 col-sm-6 col-6">
-                    <div className="service-box box-mb">
-                      <figure className="service-marketicon">
+                    <div className="gwc_domo-box box-mb">
+                      <figure className="gwc_domo-marketicon">
                         <img src="./assets/images/service-marketicon.png" alt="" className="img-fluid" />
                       </figure> 
                       <div className="font-weight-bold" style={{fontSize: "1.3rem"}}>200+ Domo Certified Experts</div>
                       {/* <p className="text-size-18">Our team consists of highly skilled professionals, certified across Domo’s ecosystem—ensuring exceptional service delivery and expertise.</p> */}
-                      {/* <a className="arrow text-decoration-none" href="./service.html"><i className="circle fa-thin fa-arrow-right"></i></a> */}
                     </div>   
                   </div>
                   <div className="col-lg-6 col-md-6 col-sm-6 col-6">
                     <div className="box-top">
-                      <div className="service-box box-mb">
-                        <figure className="service-producticon">
+                      <div className="gwc_domo-box box-mb">
+                        <figure className="gwc_domo-producticon">
                           <img src="./assets/images/service-producticon.png" alt="" className="img-fluid" />
                         </figure>
                         <div className="font-weight-bold" style={{fontSize: "1.3rem"}}>100,000+ Hours of Delivery</div>
                         {/* <p className="text-size-18">With over 100K hours dedicated to building and scaling Domo solutions, we’ve set the standard for quality, speed, and innovation.</p> */}
-                        {/* <a className="arrow text-decoration-none" href="./service.html"><i className="circle fa-thin fa-arrow-right"></i></a> */}
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className="row">
                   <div className="col-lg-6 col-md-6 col-sm-6 col-6">
-                    <div className="service-box">
-                      <figure className="service-designicon">
+                    <div className="gwc_domo-box">
+                      <figure className="gwc_domo-designicon">
                         <img src="./assets/images/service-designicon.png" alt="" className="img-fluid" />
                       </figure>
                       <div className="font-weight-bold" style={{fontSize: "1.3rem"}}>300+ Tailored Solutions Delivered</div>
                       {/* <p className="text-size-18">We have successfully delivered over 300 innovative Domo solutions, including dashboards, apps, integrations, and AI-driven analytics—customized to meet unique business needs.</p> */}
-                      {/* <a className="arrow text-decoration-none" href="./service.html"><i className="circle fa-thin fa-arrow-right"></i></a> */}
                     </div>
                   </div>
                   <div className="col-lg-6 col-md-6 col-sm-6 col-6">
                     <div className="box-top">
-                      <div className="service-box">
-                        <figure className="service-dataicon">
+                      <div className="gwc_domo-box">
+                        <figure className="gwc_domo-dataicon">
                           <img src="./assets/images/service-dataicon.png" alt="" className="img-fluid" />
                         </figure>
                         <div className="font-weight-bold" style={{fontSize: "1.3rem"}}>Award-Winning Innovation</div>
                         {/* <p className="text-size-18">Our focus on crafting cutting-edge solutions and solving complex challenges has solidified our reputation as Domo’s go-to partner.</p> */}
-                        {/* <a className="arrow text-decoration-none" href="./service.html"><i className="circle fa-thin fa-arrow-right"></i></a> */}
                       </div>
                     </div>
                   </div>
@@ -193,8 +191,8 @@ const HomePage = () => {
               </div>
             </div>
             <div className="col-lg-6 col-md-12 col-sm-12 col-12 order-md-2 order-1">
-              <div className="service_content position-relative" data-aos="fade-right" style={{paddingTop: "120px" }}>
-                <figure className="service-rightcircle mb-0">
+              <div className="gwc_domo_content position-relative" data-aos="fade-right" style={{paddingTop: "120px" }}>
+                <figure className="gwc_domo-rightcircle mb-0">
                   <img src="./assets/images/service-rightcircle.png" alt="" className="img-fluid" />
                 </figure>
                 {/* <h6>Our Services</h6> */}
@@ -208,7 +206,7 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-        <figure className="service-rightlayer mb-0">
+        <figure className="gwc_domo-rightlayer mb-0">
           <img src="./assets/images/service-rightlayer.png" alt="" className="img-fluid" />
         </figure>
       </section>
