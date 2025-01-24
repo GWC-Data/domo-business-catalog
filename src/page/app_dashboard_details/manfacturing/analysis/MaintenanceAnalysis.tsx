06ab514a59a4c7bca33343a03ff40b53100dd928
Aarthi0413 <Aarthi.Murugesan@gwcdata.ai>
2025-01-24 10:45:16 +0530

    cards

--- src/page/app_dashboard_details/manfacturing/analysis/MaintenanceAnalysis.tsx
+++ src/page/app_dashboard_details/manfacturing/analysis/MaintenanceAnalysis.tsx
@@ -1,7 +1,7 @@
 import Icon1 from "../icons/Inventory Turnover Analysis.png";
 import Icon2 from "../icons/Stock Level Analysis.png";
 import Icon3 from "../icons/Lead Time Analysis.png";
-import "./style.css";
+import "./cardstyle4.css";
 
 const MaintenanceAnalysis = () => {
     return (
